b3b24910c9e927804082bb0ed3bf12fc2edd1324
Phumi Tshidi <phumitshidi@gmail.com>
2024-02-04 17:35:47 +0000

    ExtraQ

---
+++ week02/nameAndAge.py
@@ -0,0 +1,11 @@
+# nameAndAge.py
+# this program prints out name and age
+# Author: Phumi
+
+'''
+name = input ("Enter your name:")
+print (f'Hello {name}, your age is 21.')
+'''
+
+name = input ("Enter your name:")
+print (f'Hello {name},  your age is 21.')

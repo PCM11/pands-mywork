442f00aa532f78593a5762d452d59b1d896ba412
PCM11 <157810147+PCM11@users.noreply.github.com>
2024-02-10 22:31:28 +0000

    Delete week03 directory

--- week03/sub.py
+++
@@ -1,9 +0,0 @@
-# sub.py
-# Program to subtract one number from another
-# Author: Phumi
-
-
-x = int(input("Enter first number: "))
-y = int(input("Enter second number: "))
-answer= x-y
-print("{} minus {} is {} ".format (x, y, answer))

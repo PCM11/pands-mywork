689a994172022c693e3c0f4695251476d9b92e84
Phumi Tshidi <phumitshidi@gmail.com>
2024-02-10 20:53:21 +0000

    week 03

---
+++ week03/sub.py
@@ -0,0 +1,9 @@
+# sub.py
+# Program to subtract one number from another
+# Author: Phumi
+
+
+x = int(input("Enter first number: "))
+y = int(input("Enter second number: "))
+answer= x-y
+print("{} minus {} is {} ".format (x, y, answer))
